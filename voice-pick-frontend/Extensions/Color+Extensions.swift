5d1952fc4a7c252868a3801f36c4efa561131305
PMolnes <pettermolnes@icloud.com>
2023-02-10 14:27:49 +0100

    style: add colors to assets

---
+++ voice-pick-frontend/Extensions/Color+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Color+Extensions.swift
+//  voice-pick-frontend
+//
+//  Created by Petter Molnes on 10/02/2023.
+//
+
+import SwiftUI
+
+extension Color {
+		static let foregroundColor = Color("foregroundColor")
+		static let backgroundColor = Color("backgroundColor")
+		static let traceLightYellow = Color("trace-light-yellow")
+		static let traceMediYellow = Color("trace-medi-yellow")
+		static let traceDarkYellow = Color("trace-dark-yellow")
+		static let snow = Color("snow")
+		static let glass = Color("glass")
+		static let mist = Color("mist")
+		static let cement = Color("cement")
+		static let brick = Color("brick")
+		static let mountain = Color("mountain")
+		static let stone = Color("stone")
+		static let dusk = Color("dusk")
+		static let night = Color("night")
+		static let success = Color("success")
+		static let error = Color("error")
+}

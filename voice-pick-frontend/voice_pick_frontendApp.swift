ab955544a02a81f17b8cb21048c6fd6ff3121e16
PMolnes <pettermolnes@icloud.com>
2023-02-10 11:58:50 +0100

    initial commit

---
+++ voice-pick-frontend/voice_pick_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_pick_frontendApp.swift
+//  voice-pick-frontend
+//
+//  Created by Petter Molnes on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct voice_pick_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a5182de78b1c158f129bb012d27cde5d48e71634
PMolnes <pettermolnes@icloud.com>
2023-02-15 14:34:26 +0100

    Closes VP-126: add Roboto font to the project

--- voice-pick-frontend/Extensions/Font+Extension.swift
+++ voice-pick-frontend/Extensions/Font+Extension.swift
@@ -9,13 +9,13 @@ import Foundation
 import SwiftUI
 
 extension Font {
-		static let header1 = Font.custom("Roboto", size: 34)
-		static let header2 = Font.custom("Roboto", size: 20)
-		static let body = Font.custom("Roboto", size: 16)
-		static let guidelineHeading = Font.custom("Roboto", size: 70)
-		static let button = Font.custom("Roboto", size: 16)
-		static let bodyMedium = Font.custom("Roboto", size: 16)
-		static let bodyBold = Font.custom("Roboto", size: 16)
-		static let label = Font.custom("Roboto", size: 11)
-		static let watchButton = Font.custom("Roboto", size: 11)
+		static let header1 = Font.custom("Roboto-Bold", size: 34)
+		static let header2 = Font.custom("Roboto-Bold", size: 20)
+		static let customBody = Font.custom("Roboto-Regular", size: 16)
+		static let guidelineHeading = Font.custom("Roboto-Bold", size: 70)
+		static let button = Font.custom("Roboto-Medium", size: 16)
+		static let bodyMedium = Font.custom("Roboto-Medium", size: 16)
+		static let bodyBold = Font.custom("Roboto-Bold", size: 16)
+		static let label = Font.custom("Roboto-Regular", size: 11)
+		static let watchButton = Font.custom("Roboto-Medium", size: 11)
 }

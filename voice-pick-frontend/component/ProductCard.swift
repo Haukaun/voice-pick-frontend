a9cea2738865eec3d2e53f54d11c0f36461dd2c2
Haukaun <haakonfs@hotmail.com>
2023-02-20 11:53:34 +0000

    Closes VP-148

--- voice-pick-frontend/component/ProductCard.swift
+++ voice-pick-frontend/component/ProductCard.swift
@@ -23,33 +23,57 @@ struct ProductCard: View, Hashable {
 			VStack(alignment: .leading) {
 				VStack(alignment: .leading){
 					Paragraph("Vare")
+						.lineLimit(1)
+						.truncationMode(.tail)
 					Paragraph("\(varer)")
+						.lineLimit(1)
+						.truncationMode(.tail)
 						.bold()
 				}
 				HStack(spacing: 25) {
 					VStack (alignment: .leading){
 						Paragraph("Lokasjon")
+							.lineLimit(1)
+							.truncationMode(.tail)
 						Paragraph("\(lokasjon)")
+							.lineLimit(1)
+							.truncationMode(.tail)
 							.bold()
 					}
 					VStack (alignment: .leading){
 						Paragraph("Antall")
+							.lineLimit(1)
+							.truncationMode(.tail)
 						Paragraph("\(antall)")
+							.lineLimit(1)
+							.truncationMode(.tail)
 							.bold()
 					}
 					VStack (alignment: .leading){
 						Paragraph("Vekt")
+							.lineLimit(1)
+							.truncationMode(.tail)
 						Paragraph("\(vekt)")
+							.lineLimit(1)
+							.truncationMode(.tail)
 							.bold()
 					}
 					VStack (alignment: .leading){
 						Paragraph("Type")
+							.lineLimit(1)
+							.truncationMode(.tail)
 						Paragraph("\(pakkeType)")
+							.lineLimit(1)
+							.truncationMode(.tail)
 							.bold()
 					}
 					VStack(alignment: .leading){
 						Paragraph("Status")
+							.lineLimit(1)
+							.truncationMode(.tail)
 						Paragraph("\(status)")
+							.lineLimit(1)
+							.truncationMode(.tail)
 							.bold()
 					}
 				}

2fea5c33ce4c9bc324b9bf0c1db4f42aa4f4d516
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2023-03-09 14:03:57 +0000

    Resolve VP-158 "Implement pluck flow"

---
+++ voice-pick-frontend/feature/pluck/models/PluckPages.swift
@@ -0,0 +1,17 @@
+//
+//  PluckPages.swift
+//  voice-pick-frontend
+//
+//  Created by Joakim Edvardsen on 03/03/2023.
+//
+
+import Foundation
+
+enum PluckPages {
+	case LOBBY
+	case INFO
+	case LIST_VIEW
+	case SINGLE_VIEW
+	case COMPLETE
+	case DELIVERY
+}

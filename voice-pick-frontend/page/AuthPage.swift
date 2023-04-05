22ab558c6d230aff01f5afab35aae855553c382b
Mateusz Picheta <m.picheta@icloud.com>
2023-04-05 15:42:30 +0000

    VP-39 add logic and alert on invalid email

--- voice-pick-frontend/page/AuthPage.swift
+++ voice-pick-frontend/page/AuthPage.swift
@@ -11,9 +11,14 @@ struct AuthPage: View {
 	
 	init() {
 		self.authMode = AuthMode.login
+        self.emailValue = ""
 	}
 	
-	@State var authMode: AuthMode;
+    let requestService = RequestService()
+    @State var authMode: AuthMode;
+    @State var emailValue: String;
+    @State var showAlert = false;
+    @State var errorMessage = "";
 	
 	/**
 	 Swaps between sign up and log in mode for the form.
@@ -29,6 +34,53 @@ struct AuthPage: View {
 			}
 		}
 	}
+    
+    /*
+     Send Email that will get its password reset
+     */
+    func sendResetPasswordMail(){
+        if Validator.shared.isValidEmail(emailValue) {
+            requestService.post(path: "/auth/reset-password", body: emailValue, responseType: String.self, completion: { result in
+                switch result {
+                case .success(let response):
+                    print(response)
+                    break
+                case .failure(let error as RequestError):
+                    handleError(errorCode: error.errorCode)
+                    break
+                default:
+                    break
+                }
+            })
+        } else {
+            errorMessage = "Invalid Email";
+            showAlert = true;
+        }
+    }
+    
+    /*
+     Error handling
+     */
+    func handleError(errorCode: Int) {
+        switch errorCode {
+        case 400:
+            showAlert = true
+            errorMessage = "Something went wrong, are you sure this is the correct email?"
+            break
+        case 500:
+            showAlert = true
+            errorMessage = "Something went wrong while sending the email"
+            break
+        case 404:
+            showAlert = true
+            errorMessage = "Something went wrong, user not found"
+            break
+        default:
+            showAlert = true;
+            errorMessage = "Something went wrong, please exit the application and try again, or report a bug."
+            break
+        }
+    }
 	
 	var body: some View {
 		VStack {
@@ -37,8 +89,10 @@ struct AuthPage: View {
 				Text("Voice Pick").font(.header1).foregroundColor(.foregroundColor).offset(y: -20)
 			}
 			Spacer()
-			AuthForm(authMode: $authMode)
-			Text("Forgot password?").font(.label).frame(maxWidth: .infinity, alignment: .trailing)
+            AuthForm(emailValue: $emailValue, authMode: $authMode)
+            Button("Forgot password?") {
+                sendResetPasswordMail()
+            }.font(.label).frame(maxWidth: .infinity, alignment: .trailing).foregroundColor(.black)
 			HStack {
 				Text("Dont have an account?")
 				Button(authMode == AuthMode.signup ? "Sign in here" : "Sign up here", action: switchAuthMode)
@@ -52,6 +106,7 @@ struct AuthPage: View {
 		}
 		.padding(50)
 		.background(Color.backgroundColor)
+        .alert("Reset Password", isPresented: $showAlert, actions: {}, message: { Text(errorMessage)})
 	}
 }
 
